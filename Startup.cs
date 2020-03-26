245b710bbc1b79dfe3be4c3ee3f9496c94ec4e9c
swathisinisetti <41137371+swathisinisetti@users.noreply.github.com>
2020-03-27 01:27:42 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using WebApplication1.Models;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication1.Startup))]
+namespace WebApplication1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateUserAndRoles();
+        }
+        public void CreateUserAndRoles() 
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            if (!roleManager.RoleExists("Superadmin")) 
+            {
+                var role = new IdentityRole("SuperAdmin");
+                roleManager.Create(role);
+                var user = new ApplicationUser();
+                user.UserName = "Admin@gmail.com";
+                user.Email = "Admin@gmail.com";
+                string pwd = "Admin123";
+          }
+        }
+    }
+}
